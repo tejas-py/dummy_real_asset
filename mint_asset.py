6c571b9f85b884420632dca63a8eef4c17141447
tejaswebmob <tejas@webmobinfo.ch>
2022-04-04 14:42:51 +0530

    Asset Mint

--- mint_asset.py
+++ mint_asset.py
@@ -2,14 +2,12 @@ from algosdk import mnemonic
 from algosdk.future.transaction import *
 from algod_connection import *
 
-
 # Declaring Mnemonics of accounts for minting assets
 mnemonic1 = "valley fiction rescue truth useful lady amused submit boost eight trash abandon dust smoke ranch tribe regular sphere obtain picnic cruise embark glimpse abstract upset"
 mnemonic2 = "hood route distance bargain element drink ripple worth wink brown puzzle fox cannon banana south hollow gospel hub correct build under run style able receive"
 
 
 def mint_asset(client, private_key1, private_key2, total_units, unit_name, asset_name, note, url):
-
     params = client.suggested_params()
 
     public_address1 = account.address_from_private_key(private_key1)
@@ -41,13 +39,12 @@ def mint_asset(client, private_key1, private_key2, total_units, unit_name, asset
 
 
 if __name__ == "__main__":
-
     algod_client = algo_conn()
 
     creator_private_key = mnemonic.to_private_key(mnemonic1)
     manager_private_key = mnemonic.to_private_key(mnemonic2)
 
-
-    asset = mint_asset(algod_client, creator_private_key, manager_private_key,1,"DLT",'algorand','real','www.webmobinfo.ch')
+    asset = mint_asset(algod_client, creator_private_key, manager_private_key, 1, "DLT", 'algorand', 'real',
+                       'www.webmobinfo.ch')
 
     print(asset)
